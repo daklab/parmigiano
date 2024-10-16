062da7f69a218d199958953bd5e87dc7c8320b67
Anjali Das <adas@pe2-login01.c.nygenome.org>
2024-10-16 15:00:36 -0400

    adding performance

---
+++ src/performance_metrics.py
@@ -0,0 +1,44 @@
+import sklearn
+from sklearn import metrics 
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+import numpy as np
+import pandas as pd
+import torch
+
+
+def predict_pergene(data, params, posterior_stats, alpha, Z_norm, group):
+    num_indivs = data.G[group].shape[0]
+    lambda_ = ((data.Z.T * data.maf_weights).T.matmul(data.tau)) * posterior_stats['w_g']
+    beta = posterior_stats['rho'] * lambda_ + (1-posterior_stats['rho']) * lambda_ * Z_norm['mean']
+    Gbeta = (data.G[group]).matmul(beta) 
+    mean = torch.sigmoid(torch.matmul(data.X[group], alpha).reshape(-1,1) + Gbeta.reshape(-1,1)).view(num_indivs) 
+    return mean
+    
+
+def predict_joint(data, params, posterior_stats, group):
+    num_indivs = data.G[group].shape[0]
+    performance = {}
+    performance['AUC'] = {}
+    performance['ACC'] = {}
+    
+    tau = torch.tensor(posterior_stats['tau']['mean'], dtype = torch.float32)
+    w_g = torch.tensor(posterior_stats['w_g']['mean'], dtype = torch.float32)
+    rho_g = torch.tensor(posterior_stats['rho_g']['mean'], dtype = torch.float32)
+    for gene in range(data.num_genes):
+        Z_norm = torch.tensor(posterior_stats[f"Z_{data.genes[gene]}"]['mean'], dtype = torch.float32)
+        alpha = torch.tensor(posterior_stats[f'alpha_{data.genes[gene]}']['mean'], dtype = torch.float32)
+        lambda_ = ((data.Zs[data.gene_indices==gene].T * data.maf_weights[data.gene_indices == gene]).T.matmul(tau)) * w_g[gene] 
+        beta = rho_g[gene]  * lambda_ + (1-rho_g[gene]) * lambda_ * Z_norm
+        Gbeta = data.G[group][:,data.gene_indices==gene].matmul(beta)
+        preds = torch.sigmoid(torch.matmul(data.X[group], alpha).reshape(-1,1) + Gbeta.reshape(-1,1)).view(num_indivs)
+        if params['simulate']:
+            fpr, tpr, thresholds = roc_curve(np.array(data.AD_status[group][data.genes[gene]]), preds.detach().numpy())
+            aucs[data.genes[gene]] = auc(fpr, tpr)
+            accs[data.genes[gene]] = ((preds > 0.5).float().detach().numpy() == np.array(data.AD_status[group][data.genes[gene]])).sum() / len(preds)
+        else:
+            fpr, tpr, thresholds = roc_curve(np.array(data.AD_status[group]), preds.detach().numpy())
+            performance['AUC'][data.genes[gene]] = auc(fpr, tpr)
+            performance['ACC'][data.genes[gene]] = ((preds > 0.5).float().detach().numpy() == np.array(data.AD_status[group])).sum() / len(preds)
+    return performance
